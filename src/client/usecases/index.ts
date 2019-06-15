eace504d67ed648700b3afc47dfa1ac75d583161
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-06-15 14:51:42 +0900

    Create usecases with thunk

---
+++ src/client/usecases/index.ts
@@ -0,0 +1,25 @@
+import { Action } from 'redux';
+import { ThunkDispatch } from 'redux-thunk';
+
+import { fetchingCount, fetchedCount, increment, decrement } from 'client/actions';
+import { ExampleAPI } from 'client/services';
+
+export const asyncIncrement = () => {
+  return (dispatch: ThunkDispatch<{}, {}, Action>) => {
+    dispatch(fetchingCount());
+    ExampleAPI.call().then(() => {
+      dispatch(increment());
+      dispatch(fetchedCount());
+    });
+  };
+}
+
+export const asyncDecrement = () => {
+  return (dispatch: ThunkDispatch<{}, {}, Action>) => {
+    dispatch(fetchingCount());
+    ExampleAPI.call().then(() => {
+      dispatch(decrement());
+      dispatch(fetchedCount());
+    });
+  };
+}
