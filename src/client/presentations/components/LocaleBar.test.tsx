4a5ea3270614a2017471a7c139994629f8962663
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-08-04 13:14:07 +0900

    Remove needless components

--- src/client/presentations/components/LocaleBar.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import renderer from 'react-test-renderer';
-
-import { LocaleBar } from './LocaleBar';
-
-it('Renders correctly', () => {
-  const tree = renderer.create(<LocaleBar onEnglishLocaleClick={() => {}} onJapaneseLocaleClick={() => {}} />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
--- src/client/presentations/components/Navigation.tsx
+++ src/client/presentations/components/Navigation.tsx
@@ -1,21 +1,26 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
-import { LocaleBar } from 'client/containers/LocaleBar';
-
 export function Navigation() {
   return (
     <nav>
-      <LocaleBar />
+      <ul>
+        <li>
+          <Link to="/">English</Link>
+        </li>
+        <li>
+          <Link to="/ja">日本語</Link>
+        </li>
+      </ul>
       <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/about/">About</Link>
+          <Link to="/about">About</Link>
         </li>
         <li>
-          <Link to="/users/">Users</Link>
+          <Link to="/users">Users</Link>
         </li>
       </ul>
     </nav>
